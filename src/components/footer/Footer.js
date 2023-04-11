75c03e007bc8c86bb5a179c5a8d3221f2d0d179e
akumar <akumar@comtronusa.com>
2023-04-11 07:32:20 -0400

    added footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,7 @@
 import styles from "./footer.module.css";
-import Logo from "../../shared/assets/logo1.png";
+import Logo from "../../shared/assets/logo2.png";
 import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
 
 const Footer = () => {
   return (
@@ -9,6 +10,16 @@ const Footer = () => {
         <div className={styles["logo-wrapper"]}>
           <img src={Logo} alt={"Rabbit Mart"} />
         </div>
+        <Typography>
+          Welcome to our NaturalGanic, where we offer high-quality organic and
+          inorganic products that are safe, healthy, and eco-friendly. From
+          personal care to household essentials, we curate only the best natural
+          and responsibly manufactured options. We're committed to fair trade
+          and ethical sourcing, and we're dedicated to making sustainable living
+          easy and convenient for you. Shop with us today and experience the
+          difference that high-quality organic and inorganic products can make
+          in your life.
+        </Typography>
         <div className={styles["pages-wrapper"]}>
           <div className={styles["pages"]}>
             <div className={styles["page-title"]}>Shopping</div>
